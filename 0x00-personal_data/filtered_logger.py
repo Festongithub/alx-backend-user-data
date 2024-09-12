1dffd181b2d19a0e89c52a492ef777996f54dac3
Festongithub <fkemboik@gmail.com>
2024-09-12 09:21:31 +0000

    Logging data user

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,4 +7,10 @@ from typing import List, Union
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """filter data"""
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+        return message
