c7339dcc06a083a9f40684c66a16a9cd5e938ece
Festongithub <fkemboik@gmail.com>
2024-09-12 09:58:54 +0000

    Logging data user

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,6 +21,19 @@ def filter_datum(fields: List[str], redaction: str,
         return message
 
 
+def get_logger() -> logging.Logger:
+    """Returns a logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHanlder()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
