6a9ff71daa010756fff92edafe1176ac2f6229a6
Festongithub <Fkemboik@gmail.com>
2024-09-24 13:10:48 +0000

    User auth service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Basic flask App"""
 from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -12,6 +14,25 @@ def index() -> str:
     """
     text = {"message": "Bienvenue"}
     return jsonify(text)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """"
+    Register a new user
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    msg = {"email": email, "message": message}
+    return jsonify(msg)
 
 
 if __name__ == "__main__":
